ada042b8d70f9a8e93eccdbbd0ad4d44c922ad39
Bethany Long <longb4@packetdisarray.com>
2015-04-13 18:45:29 -0700

    nicer css

--- source/conf.py
+++ source/conf.py
@@ -32,7 +32,7 @@ import shlex
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['_templates', 'source/_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -139,7 +139,7 @@ html_title = "{} {}".format(project, release)
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', 'source/_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -202,7 +202,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'CSCI330TheGoodPartsdoc'
+htmlhelp_basename = 'CSCI330TheGoodParts'
 
 # -- Options for LaTeX output ---------------------------------------------
 
